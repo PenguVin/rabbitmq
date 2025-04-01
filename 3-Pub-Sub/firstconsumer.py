dc7ab724ea492afbb044626a24093e24624df885
viransh <viranshcloudprofessional@gmail.com>
2025-04-01 11:22:28 +0530

    test2

--- 3-Pub-Sub/firstconsumer.py
+++ 3-Pub-Sub/firstconsumer.py
@@ -11,9 +11,9 @@ channel = connection.channel()
 
 channel.exchange_declare(exchange='pubsub', exchange_type='fanout')
 
-queue = channel.queue_declare(queue='', exclusive=True)
+queue = channel.queue_declare(queue='', exclusive=True)# once connection is closed , the queue can be deleted. also no name is given to the queue.
 
-channel.queue_bind(exchange='pubsub', queue=queue.method.queue)
+channel.queue_bind(exchange='pubsub', queue=queue.method.queue) # binding the queue to the consumer. If the queue is not bound to the consumer then no message will be received by the consumer since it wont have any queue to reference to.
 
 channel.basic_consume(queue=queue.method.queue, auto_ack=True,
     on_message_callback=on_message_received)
